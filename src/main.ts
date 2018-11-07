4a54ff59289fb2ba0dd54652a6cddeee234f6df5
mandorin <pitsanu_pipitgool@hotmail.com>
2018-11-07 14:23:54 +0700

    Config document for api

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import 'reflect-metadata'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+  await genDoc(app)
+  await app.listen(3000)
 }
-bootstrap();
+
+async function genDoc(app) {
+  const apiGennerateDoc = new DocumentBuilder()
+    .setTitle('Backend Educationinsurance Api')
+    .setDescription(
+      'This is document tell about how to call restfullapi and datamodel + can test to call api',
+    )
+    .setVersion('0.0.1')
+    .addTag('Educationinsurance')
+    .setBasePath('/api')
+    .addBearerAuth('Authorization', 'header')
+    .build()
+
+  const swaggerDocument = SwaggerModule.createDocument(app, apiGennerateDoc)
+
+  app.use('/api/docs/swagger.json', (req, res) => {
+    res.send(swaggerDocument)
+  })
+
+  SwaggerModule.setup('api/v0.0.1/doc', app, swaggerDocument, {
+    swaggerOptions: {
+      docExpansion: 'list',
+      filter: true,
+      showRequestDuration: true,
+    },
+  })
+}
+
+bootstrap()
