e7bde18bb2f4e922c7c0ddc43547510b432b0b72
mandorin <pitsanu_pipitgool@hotmail.com>
2018-11-07 22:15:03 +0700

    Add salt to encrypt password ☄️

--- src/module/user/controllers/user.controller.ts
+++ src/module/user/controllers/user.controller.ts
@@ -1,24 +1,45 @@
-import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
-import { ApiUseTags, ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { UserService } from '../services/user.service';
-import { CreateUserDTO, UserDTO } from '../userdto/user.dto';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common'
+import {
+  ApiUseTags,
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger'
+import { UserService } from '../services/user.service'
+import { CreateUserDTO, UserDTO } from '../userdto/user.dto'
 
 @Controller('user')
 @ApiUseTags('User')
-
+@ApiBearerAuth()
 export class UserController {
-    constructor(private readonly userService: UserService) { }
+  constructor(private readonly userService: UserService) {}
 
-    @Post('register')
-    @ApiOperation({ title: 'เพิ่มผู้ใช้งาน', description: 'เพิ่มผู้ใช้งานเพื่อเข้าใช้งานระบบ', operationId: 'Create Users' })
-    @ApiResponse({ status: HttpStatus.OK, type: UserDTO, description: 'เพิ่มผู้ใช้งานสำเร็จ' })
-    async create(@Body() userBody: CreateUserDTO) {
-        try {
-            const userRes = await this.userService.createUser(userBody)
-            return userRes.toDto()
-
-        } catch (error) {
-            throw new HttpException({ message: 'ไม่สามารถเพิ่มผู้ใช้งานได้' }, HttpStatus.BAD_REQUEST)
-        }
+  @Post('register')
+  @ApiOperation({
+    title: 'เพิ่มผู้ใช้งาน',
+    description: 'เพิ่มผู้ใช้งานเพื่อเข้าใช้งานระบบ',
+    operationId: 'Create Users',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    type: UserDTO,
+    description: 'เพิ่มผู้ใช้งานสำเร็จ',
+  })
+  async create(@Body() userBody: CreateUserDTO) {
+    try {
+      const userRes = await this.userService.createUser(userBody)
+      return userRes.toDto()
+    } catch (error) {
+      throw new HttpException(
+        { message: 'ไม่สามารถเพิ่มผู้ใช้งานได้' },
+        HttpStatus.BAD_REQUEST,
+      )
     }
+  }
 }
--- src/module/user/services/user.service.ts
+++ src/module/user/services/user.service.ts
@@ -1,36 +1,52 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { User } from '../entities/user.entity';
-import { Repository } from 'typeorm';
-import { CreateUserDTO } from '../userdto/user.dto';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from '../entities/user.entity'
+import { Repository } from 'typeorm'
+import { CreateUserDTO } from '../userdto/user.dto'
+import { genSalt, hash } from 'bcrypt'
 
 @Injectable()
-
 export class UserService {
-    constructor(@InjectRepository(User)
-    private readonly userRepo: Repository<User>) { }
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
 
-    async createUser(body: CreateUserDTO) {
+  async createUser(body: CreateUserDTO) {
+    const { username, password } = body
 
-        const { username, password } = body
-
-        if (!username) {
-            throw new HttpException({ message: 'ไม่สามารถเพิ่มผู้ใช้งานได้', username: 'กรุณากรอกชื่อผู้ใช้งาน' }, HttpStatus.BAD_REQUEST)
-        }
-
-        if (!password) {
-            throw new HttpException({ message: 'ไม่สามารถเพิ่มผู้ใช้งานได้', username: 'กรุณากรอกรหัสผ่าน' }, HttpStatus.BAD_REQUEST)
-        }
-
-        const newUser = new User()
-        newUser.username = username
-        newUser.password = password
-
-        try {
-            const user = await this.userRepo.save(newUser)
-            return user
-        } catch (error) {
-            throw new HttpException({ message: 'ไม่สามารถเพิ่มผู้ใช้งานได้', error }, HttpStatus.BAD_REQUEST)
-        }
+    if (!username) {
+      throw new HttpException(
+        {
+          message: 'ไม่สามารถเพิ่มผู้ใช้งานได้',
+          username: 'กรุณากรอกชื่อผู้ใช้งาน',
+        },
+        HttpStatus.BAD_REQUEST,
+      )
     }
+
+    if (!password) {
+      throw new HttpException(
+        {
+          message: 'ไม่สามารถเพิ่มผู้ใช้งานได้',
+          username: 'กรุณากรอกรหัสผ่าน',
+        },
+        HttpStatus.BAD_REQUEST,
+      )
+    }
+
+    const newUser = new User()
+    newUser.username = username
+    const salt = await genSalt(10)
+    newUser.password = await hash(password, salt)
+
+    try {
+      const user = await this.userRepo.save(newUser)
+      return user
+    } catch (error) {
+      throw new HttpException(
+        { message: 'ไม่สามารถเพิ่มผู้ใช้งานได้', error },
+        HttpStatus.BAD_REQUEST,
+      )
+    }
+  }
 }
