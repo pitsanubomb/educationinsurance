083574fb66f7cef45e8c021d2ac84a29c3b6b541
mandorin <pitsanu_pipitgool@hotmail.com>
2018-11-07 14:32:00 +0700

    Move file to short more

--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,13 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import 'reflect-metadata'
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { genDoc } from './document';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   await genDoc(app)
   await app.setGlobalPrefix('/api')
   await app.listen(3000)
-}
-
-async function genDoc(app) {
-  const apiGennerateDoc = new DocumentBuilder()
-    .setTitle('Backend Educationinsurance Api')
-    .setDescription(
-      'This is document tell about how to call restfullapi and datamodel + can test to call api',
-    )
-    .setVersion('0.0.1')
-    .addTag('Educationinsurance')
-    .setBasePath('/api')
-    .addBearerAuth('Authorization', 'header')
-    .build()
-
-  const swaggerDocument = SwaggerModule.createDocument(app, apiGennerateDoc)
-
-  app.use('/api/docs/swagger.json', (req, res) => {
-    res.send(swaggerDocument)
-  })
-
-  SwaggerModule.setup('api/v0.0.1/doc', app, swaggerDocument, {
-    swaggerOptions: {
-      docExpansion: 'list',
-      filter: true,
-      showRequestDuration: true,
-    },
-  })
 }
 
 bootstrap()
